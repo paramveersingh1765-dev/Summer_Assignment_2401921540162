eb834a4510b42a4b7c98963b8d0cf259df561989
Paramveer Singh <paramveer.singh1765@gmail.com>
2026-06-05 13:17:48 +0530

    Added valid palindrome

---
+++ Week_1/Day_5/palindrome.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        char [] ch=s.toCharArray();
+        StringBuilder ans=new StringBuilder();
+        for(char ele:ch){
+            if(ele>='a'&&ele<='z'||ele>='A'&&ele<='Z'||ele>='0'&&ele<='9'){
+                ans.append(Character.toLowerCase(ele));
+            }
+        }
+        String orig=ans.toString();
+        ans.reverse();
+        String rev=ans.toString();
+        return (orig.equals(rev));
+    }
+}
